02564612237eb344f5d9aa2ff2ce05d3d0231be4
jesusgm0 <gabrielmarioj1@gmail.com>
2024-07-16 23:19:55 -0300

    Estudos em Python

---
+++ udemy/listas.py
@@ -0,0 +1,14 @@
+# string = "ABCDE"
+# # Listas são mutaves
+# lista = [123, True, "Gabriel", 1.2, []]
+# print(lista)
+# lista[3] = 2.7
+# print(lista[3])
+# print(lista)
+
+
+lista = [1, 2, 3, 4]
+lista[2] = 900
+print(lista[2])
+del lista[2]
+print(lista)
---
+++ udemy/pessoa.py
@@ -0,0 +1,23 @@
+# class Pessoa:
+    
+#     def __init__(self, nome, idade, cpf):
+#         self.nome = nome
+#         self.idade = idade
+#         self.__cpf = cpf
+        
+#     def correr(self):
+#         print("Estou correndo!")
+
+#     def beber(self, bebida):
+#         # bebida = input("Qual bebida vc quer? ")
+#         if bebida == "cerveja":
+#             self.__apresentar_documento()
+#         print("bebendo")
+
+#     def __apresentar_documento(self):
+#         print(self.__cpf)
+        
+# ronaldo = Pessoa("Ronaldo", "32", "12345678911")
+# ronaldo.beber("cerveja")
+# ronaldo.beber("cocacola")
+
